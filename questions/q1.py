f1720b3a83efb98e74565315c7caff9917cb1d63
aykutaaykut <aykutaaykut.ai@gmail.com>
2022-01-29 19:43:02 +0100

    added function description and in-function parsing

--- questions/q1.py
+++ questions/q1.py
@@ -1,3 +1,6 @@
+from .utils import *
+
+
 '''
 Given a list of numbers and a number k, return whether any two numbers from
 the list add up to k. For example, given [10, 15, 3, 7] and k of 17, return 
@@ -5,7 +8,10 @@ true since 10 + 7 is 17.
 
 Bonus: Can you do this in one pass?
 '''
-def is_sum_up(arr, k):
+def is_sum_up(arr: list, k: int) -> bool:
+    arr = str2arr(arr)
+    k = int(k)
+
     cache = []
     for i in arr:
         if i in cache:
